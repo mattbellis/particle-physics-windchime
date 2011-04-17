cdd3ee54b178b715ca8a89c7f7bfc3bccc05b198
Matthew Bellis <matthew.bellis@gmail.com>
2011-04-17 00:45:54 -0700

    Added Mute/Blind buttons.

--- ppw_3D/zip_up.sh
+++ ppw_3D/zip_up.sh
@@ -5,6 +5,7 @@ set name = "ppw_3D"
 foreach os( linux macosx windows )
    set dir = "app_"$name"_"$os
    set zipfile = "app_"$name"_"$os".zip"
+   rm -rf $dir
    mv application.$os $dir
    cp -r data $dir
    zip -r $zipfile $dir
